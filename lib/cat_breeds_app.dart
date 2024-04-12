5596c231b24fc4035be01ef546fd4e1b0bf5ab8e
Daniel Osorio <tiancris01@gmail.com>
2024-04-12 13:42:30 -0500

    Feat: cats Breeds folder structure created

---
+++ lib/cat_breeds_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CatBreedsApp extends StatelessWidget {
+  const CatBreedsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
