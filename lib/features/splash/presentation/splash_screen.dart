1b38596d611131a1f666f2dfc9f717f7ca4f3e97
Daniel Osorio <tiancris01@gmail.com>
2024-04-13 08:51:56 -0500

    Feat: gif animated

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -1,17 +1,20 @@
+import 'package:cat_breeds/app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
-  static const name = 'Splash-screen';
+  static const name = 'Splash_screen';
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cat Breeds'),
-      ),
-      body: const Center(
-        child: Text('Splash Screen'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            AssetsToken.images.splashLoading.path,
+          ),
+        ],
       ),
     );
   }
