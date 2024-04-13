41fbdea3d89217846e94b806ea502d094f920414
Daniel Osorio <tiancris01@gmail.com>
2024-04-13 09:51:13 -0500

    Feat: splash screen created

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -1,20 +1,23 @@
 import 'package:cat_breeds/app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashScreen extends StatelessWidget {
-  static const name = 'Splash_screen';
+class SplashScreen extends ConsumerWidget {
+  static const name = 'Splash-screen';
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+// Introduce a delay of 2 seconds before switching to the main screen
+    Future.delayed(Duration(seconds: 2), () {
+      context.go('/breeds');
+    });
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            AssetsToken.images.splashLoading.path,
-          ),
-        ],
+      body: Center(
+        child: Image.asset(
+          AssetsToken.images.splashLoading.path,
+        ),
       ),
     );
   }
---
+++ lib/features/splash/providers/splash_provider.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'splash_provider.g.dart';
+
+@riverpod
+class Splash extends _$Splash {
+  @override
+  bool build() {
+    return true;
+  }
+}
