e13dd872a9d082f19cc6b08d231590a8f7d490e9
Daniel Osorio <tiancris01@gmail.com>
2024-04-17 08:54:10 -0500

    Feat: search feature implemented

--- lib/features/cats/presentation/widgets/delegates/search_cats_delegate.dart
+++ lib/features/cats/presentation/widgets/delegates/search_cats_delegate.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:cat_breeds/features/cats/domain/entities/cat_entity.dart';
 import 'package:cat_breeds/features/cats/presentation/widgets/cat_breeds_card.dart';
@@ -10,9 +12,42 @@ typedef CatSearchCallback = Future<Either<AppException, List<CatEntity>>>
 
 class SearchCatsDelegate extends SearchDelegate<CatEntity?> {
   final CatSearchCallback _searchCallback;
+  StreamController<Either<AppException, List<CatEntity>>> _debounceSearch =
+      StreamController.broadcast();
+
+  Timer? _debounceTimer;
 
   SearchCatsDelegate({required CatSearchCallback searchCallback})
       : _searchCallback = searchCallback;
+
+  void clearStreams() {
+    _debounceSearch.close();
+  }
+
+  void onQueryChange(String query) {
+    if (_debounceTimer?.isActive ?? false) {
+      _debounceTimer!.cancel();
+    }
+    _debounceTimer = Timer(
+      const Duration(milliseconds: 100),
+      () async {
+        if (query.isEmpty) {
+          _debounceSearch.add(
+            Left(
+              AppException(
+                message: 'Query is empty',
+                statusCode: 0,
+                identifier: 'onQueryChange',
+              ),
+            ),
+          );
+        } else {
+          final result = await _searchCallback(query: query);
+          _debounceSearch.add(result);
+        }
+      },
+    );
+  }
 
   @override
   String get searchFieldLabel => 'Search cats';
@@ -35,30 +70,87 @@ class SearchCatsDelegate extends SearchDelegate<CatEntity?> {
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-        onPressed: () => close(context, null),
+        onPressed: () {
+          clearStreams();
+          close(context, null);
+        },
         icon: const Icon(Icons.arrow_back));
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return const Text('Results');
+    onQueryChange(query);
+
+    return StreamBuilder(
+      stream: _debounceSearch.stream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('An error occurred'),
+          );
+        }
+        final cats = snapshot.data;
+        return cats!.fold(
+          (failure) => Center(
+            child: Text(failure.message),
+          ),
+          (data) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  final cat = data[index];
+                  return CatBreedCard(cat: cat);
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder(
-      future: _searchCallback(query: query),
+    onQueryChange(query);
+
+    return StreamBuilder(
+      stream: _debounceSearch.stream,
       builder: (context, snapshot) {
-        if (snapshot.data == null) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          return snapshot.data!.fold(
-            (l) => Text(l.message),
-            (r) => ListView(
-              children: r.map((cat) => CatBreedCard(cat: cat)).toList(),
-            ),
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
           );
         }
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('An error occurred'),
+          );
+        }
+        final cats = snapshot.data;
+        return cats!.fold(
+          (failure) => Center(
+            child: Text(failure.message),
+          ),
+          (data) {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  final cat = data[index];
+                  return CatBreedCard(cat: cat);
+                },
+              ),
+            );
+          },
+        );
       },
     );
   }
